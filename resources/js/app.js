c745566d719b0ce12fc259b8e68311b03e011879
achmad_syiammudin <achmad.syiammudin@lamsolusi.com>
2023-02-25 12:58:45 +0700

    app js

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,8 +15,12 @@ import App from './App.vue'
 import router from './router'
 
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI);
+import locale from 'element-ui/lib/locale/lang/en'
+
+Vue.use(ElementUI, {
+    locale
+});
+
 
 const app = new Vue({
     el: '#app',
